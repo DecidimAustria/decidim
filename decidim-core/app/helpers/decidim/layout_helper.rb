5a18d400427ed2fb5705071ee4cc942ee04ebb6c
Alexander Rusa <alex@rusa.at>
2025-05-11 11:54:55 +0200

    Remove host from variant_url()

--- decidim-core/app/helpers/decidim/layout_helper.rb
+++ decidim-core/app/helpers/decidim/layout_helper.rb
@@ -15,12 +15,12 @@ module Decidim
       return if current_organization.favicon.blank?
 
       safe_join(Decidim::OrganizationFaviconUploader::SIZES.map do |version, size|
-        favicon_link_tag(current_organization.attached_uploader(:favicon).variant_url(version, host: current_organization.host), sizes: "#{size}x#{size}")
+        favicon_link_tag(current_organization.attached_uploader(:favicon).variant_url(version), sizes: "#{size}x#{size}")
       end)
     end
 
     def apple_favicon
-      icon_image = current_organization.attached_uploader(:favicon).variant_url(:medium, host: current_organization.host)
+      icon_image = current_organization.attached_uploader(:favicon).variant_url(:medium)
       return unless icon_image
 
       favicon_link_tag(icon_image, rel: "apple-touch-icon", type: "image/png")
